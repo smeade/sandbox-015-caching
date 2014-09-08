1c41fe7c23059b5ab47be199981835e64641c16b
Scott Meade <scott@synapsoftware.com>
2014-09-08 11:13:24 -0600

    Create fake products via faker gem
    - rake db:seed

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
 # Use postgresql as the database for Active Record
@@ -25,6 +24,8 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
+
+gem 'faker', github: 'stympy/faker'
 
 group :development do
   gem "annotate"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+1000.times do |i|
+  Product.create( sku: Faker::Code.isbn, name: Faker::Commerce.product_name, description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price )
+end
