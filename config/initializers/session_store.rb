fdefe36c7d2fde168dcdef1fe0b4d7bb30f75bc7
Scott Meade <scott@synapsoftware.com>
2014-09-10 11:59:37 -0600

    Rename project to Sandbox015Caching

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sandbox-014-bourbon_session'
+Rails.application.config.session_store :cookie_store, key: '_sandbox-015-caching_session'
